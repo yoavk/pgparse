aa1161f42c51f5df8ffdea38352e311dcda983eb
Yoav Koplon <yoavkoplon@gmail.com>
2022-06-13 14:22:01 +0300

    Fix protoc in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ import sysconfig
 
 import setuptools
 from setuptools.command import build_ext
+import setuptools.command.build_py
+
 if os.environ.get('USE_CYTHON') in ['1', 'true', 'TRUE', 'Y']:
     from Cython.Build import cythonize
 else:
@@ -28,11 +30,6 @@ EXT_MODULES = [
 class BuildExt(build_ext.build_ext):
     def run(self):
         return_code = subprocess.call(
-            ['protoc', '-I', f'{LIBPG_QUERY}/protobuf', '--python_out', './pgparse_proto', f'{LIBPG_QUERY}/protobuf/pg_query.proto'])
-        if return_code:
-            sys.stderr.write('Failed to run protoc')
-            sys.exit(return_code)
-        return_code = subprocess.call(
             ['make', '-C', LIBPG_QUERY, 'build'])
         if return_code:
             sys.stderr.write('libpg_query failed to build')
@@ -40,8 +37,22 @@ class BuildExt(build_ext.build_ext):
         super().run()
 
 
+class BuildPyCommand(setuptools.command.build_py.build_py):
+  """Build protobuf package"""
+
+  def run(self):
+      return_code = subprocess.call(
+          ['protoc', '-I', f'{LIBPG_QUERY}/protobuf', '--python_out', './pgparse_proto',
+           f'{LIBPG_QUERY}/protobuf/pg_query.proto'])
+      if return_code:
+          sys.stderr.write('Failed to run protoc')
+          sys.exit(return_code)
+      setuptools.command.build_py.build_py.run(self)
+
+
 setuptools.setup(
-    cmdclass={'build_ext': BuildExt},
+    cmdclass={'build_ext': BuildExt,
+              'build_py': BuildPyCommand},
     packages=['pgparse_proto'],
     ext_modules=EXT_MODULES if cythonize is None else cythonize(EXT_MODULES),
     zip_safe=False)
